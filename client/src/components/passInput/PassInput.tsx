9c171a987411061e9949533a216f3631f5a97c81
Shkatylo4ka <Shkatylo4ka@gmail.com>
2024-10-30 08:26:12 +0300

    login

--- client/src/components/passInput/PassInput.tsx
+++ client/src/components/passInput/PassInput.tsx
@@ -15,7 +15,27 @@ const PassInput = ({
         name={name}
         dependencies={dependencies}
         hasFeedback
-        rules={[{}]}
+        rules={[{
+            required: true,
+            message: "Обязательное поле"
+        }, ({getFieldValue}) => ({
+            validator(_,value){
+                if (!value) {
+                    return Promise.resolve();
+                }
+                if (name === 'confirmPassword') {
+                    if (!value || getFieldValue(('password')) === value) {
+                     return Promise.resolve();
+                    }
+                    return Promise.reject(new Error('Пароли должны совпадать'))
+                } else {
+                    if (value.length < 6) {
+                        return Promise.reject(new Error('Пароль должен быть больше 6 символов'))
+                    }
+
+                }
+            }
+        })]}
     >
         <Input.Password placeholder={placeholder} size='large'/>
     </Form.Item>
